dd881d3309e82a59842066db65cbb65b987e4143
ivan-meling <ivan.meling.ibb@gmail.com>
2024-08-26 10:49:57 -0700

    Fix sql logic

--- admin/layout/parte1.php
+++ admin/layout/parte1.php
@@ -27,8 +27,11 @@ if (isset($_SESSION['sesion_email'])) {
                        INNER JOIN permisos as per ON per.id_permiso = rolper.permiso_id 
                        INNER JOIN roles as rol ON rol.id_rol = rolper.rol_id 
                        where rolper.estado = '1' ";
-                       
-    $roles_permisos = $pdo->prepare($sql_roles_permisos)->execute()->fetchAll(PDO::FETCH_ASSOC);
+
+    $query_roles_permisos = $pdo->prepare($sql_roles_permisos);
+    $query_roles_permisos->execute();
+    $roles_permisos = $query_roles_permisos->fetchAll(PDO::FETCH_ASSOC);
+
     $contador_permiso = 0;
     foreach ($roles_permisos as $roles_permiso){
         if ($id_rol_sesion_usuario == $roles_permiso['rol_id']) {
